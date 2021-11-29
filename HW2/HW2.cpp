72f1d181ad6d7cbb2eb1a647ce9cf4f4a16c4ece
Andrey <89473462+Arty525@users.noreply.github.com>
2021-11-29 16:05:26 +0300

    Add files via upload

---
+++ HW2/HW2.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	float summ = 0;
+	vector<float> price {12.5, 10.0, 25.1, 72.3, 1.0, 11.5};
+	vector<int> buy{0,0,4};
+	for (int i = 0; i < buy.size(); ++i) {
+		summ += price[buy[i]];
+	}
+
+	cout << "SUMM: " << summ;	
+}
