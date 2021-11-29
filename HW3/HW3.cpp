c2b97487b8152413b761d475a18e6e68a8e2de80
Andrey <89473462+Arty525@users.noreply.github.com>
2021-11-29 16:38:40 +0300

    Add files via upload

--- HW3/HW3.cpp
+++ HW3/HW3.cpp
@@ -1,12 +1,31 @@
-﻿// HW3.cpp: определяет точку входа для приложения.
-//
-
-#include "HW3.h"
+﻿#include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main()
-{
-	cout << "Hello CMake." << endl;
+vector<int> remove(vector<int> numbers) {
+	reverse(numbers[0], numbers[numbers.size() - 1]);
+	numbers.pop_back();
+	reverse(numbers[0], numbers[numbers.size() - 1]);
+	return numbers;
+}
+
+int main() {
+	vector<int> numbers;
+	int n;
+
+	do {
+		cout << "Input number: ";
+		cin >> n;
+		if (numbers.size() == 21) {
+			numbers = remove(numbers);
+		}
+		numbers.push_back(n);
+	} while (n != -1);
+
+	for (int i = 0; i < numbers.size(); ++i) {
+		cout << numbers[i] << " ";
+	}
+
 	return 0;
 }
